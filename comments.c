d7d9324db475cb3b0eb46415b8b8fbe6611e8e79
Jeff Szuhay <42553077+jeffszuhay@users.noreply.github.com>
2021-04-10 02:13:48 -0700

    Add files via upload

---
+++ comments.c
@@ -0,0 +1,30 @@
+/* (1) A single-line C-style comment. */
+
+/* (2) A multi-line
+   C-style comment.   */
+
+/*
+ * (3) A very common way to 
+ * format a multi-line
+ * C-Style comment.
+ */
+
+/* (4) C-style comments can appear almost anywhere. */
+
+/*5*/  printf(  /* say hello */  "Hello, world!\n");
+
+/*6*/  printf( "Hello, world!\n" );  /* yay! */
+
+// (7) A C++ style comment (terminated by End-of-Line).
+
+printf( "Hello, world!\n" );  // (8) Say hello; yay!
+
+//
+// (9) A more common way 
+// of commenting with multi-line
+// C++ style comments.
+//
+
+// (10) Anything can appear after //, even /* ... */ and
+// more // after the first // but they will be 
+// ignored because they are all in the comment.
