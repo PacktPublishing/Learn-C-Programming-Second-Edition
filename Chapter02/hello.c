cf0ce7a9acd08873fa5e4fa911cd8455a341b343
Jeff Szuhay <42553077+jeffszuhay@users.noreply.github.com>
2021-05-17 11:33:12 -0700

    Add files via upload

---
+++ Chapter02/hello.c
@@ -0,0 +1,18 @@
+//
+// hello.c
+// Chapter 2
+// <book title>
+//
+// Simple "Hello, world!" program.
+//
+
+#include <stdio.h>
+
+
+int main() 
+{
+  printf( "Hello, world!\n" );
+  return 0;
+}
+
+//  <eof>
