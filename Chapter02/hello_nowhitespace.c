9838c7c774c2c3a242c1ab8a10a27ea6036b28ec
Jeff Szuhay <42553077+jeffszuhay@users.noreply.github.com>
2021-05-17 11:43:03 -0700

    Delete hello_nowhitespace.c

--- Chapter02/hello_nowhitespace.c
+++
@@ -1,9 +0,0 @@
-// hello_nowhitespace.c
-// Chapter 2
-// <book title>
-//
-// We're saving spaces but so what?
-// This is really bad programming style.
-//
-#include<stdio.h>
-int main(){printf("Hello, world!\n");return 0;}
