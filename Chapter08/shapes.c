e35564c3562ef885fa5b2e544e8ffc288ed07278
Jeff Szuhay <42553077+jeffszuhay@users.noreply.github.com>
2021-09-12 20:35:44 -0700

    Add files via upload

---
+++ Chapter08/shapes.c
@@ -0,0 +1,109 @@
+// shapes.c
+// Chapter 8
+// Learn C Programming, 2nd Edition
+//
+// Demonstrate defining and using enumerated types with 
+// the switch statement.
+// Are all 9 shapes handled everywhere?
+// This illustrated why 'default:' is important in 'switch()' statement.
+//
+// Compile with:
+//
+//    cc shapes.c -o shapes -Wall -Werror -std=c17
+
+
+#include <stdio.h>
+
+
+const char* nameTriangle  = "triangle";
+const char* nameSquare    = "square";
+const char* nameRectangle = "rectangle";
+const char* nameTrapezoid = "trapezoid";
+const char* namePentagon  = "pentagon";
+const char* nameHexagon   = "hexagon";
+const char* nameOctagon   = "octagon";
+const char* nameDecagon   = "decagon";
+const char* nameCircle    = "circle";
+const char* nameUnknown   = "unknown_name";
+
+enum shape  {
+  triangle, 
+  square, 
+  rectangle, 
+  trapezoid, 
+  pentagon, 
+  hexagon, 
+  octagon,
+  decagon,
+  circle
+};
+
+
+void        PrintShapeInfo( enum shape aShape );
+const char* GetShapeName(   enum shape aShape );
+
+
+int main( void )  {
+  PrintShapeInfo( triangle );
+  PrintShapeInfo( square );
+  PrintShapeInfo( rectangle );
+  PrintShapeInfo( trapezoid );
+  PrintShapeInfo( pentagon );
+  PrintShapeInfo( hexagon );
+  PrintShapeInfo( octagon );
+  PrintShapeInfo( decagon );
+  PrintShapeInfo( circle );
+  return 0;
+}
+
+
+void PrintShapeInfo( enum shape aShape)  {
+  int nSides = 0;
+  switch( aShape )  {
+    case triangle: 
+      nSides = 3;
+      break;
+    case square:
+    case rectangle:
+    case trapezoid:
+      nSides = 4;
+      break;
+    // missing something?
+    case hexagon:
+      nSides = 6;
+      break;
+    case  octagon:
+      nSides = 8;
+      break; 
+    // missing something?
+    case circle:
+      printf( "A circle has an infinite number of sides\n" );
+      return;
+      break;
+    default:
+      printf( "UNKNOWN SHAPE TYPE: %s\n" , GetShapeName( aShape ) );
+      return;
+      break;
+  }
+  printf( "A %s has %d sides\n" , GetShapeName( aShape) , nSides );
+}
+
+const char* GetShapeName( enum shape aShape)  {
+  const char* name = nameUnknown;
+  switch( aShape )  {
+    case triangle:  name = nameTriangle;  break;
+    case square:    name = nameSquare;    break;
+    case rectangle: name = nameRectangle; break;
+    case trapezoid: name = nameTrapezoid; break;
+    case pentagon:  name = namePentagon;  break;
+    case hexagon:   name = nameHexagon;   break;
+    case octagon:   name = nameOctagon;   break;
+    case circle:    name = nameCircle;    break;
+    // missing something?
+    default:        name = nameUnknown;   break;
+  }
+  return name;
+}
+
+  //  eof
+  
---
+++ Chapter08/styles.c
@@ -0,0 +1,35 @@
+  // styles.c
+  // Chapter 8
+  // Learn C Programming, 2nd Edition  
+  //
+  // Compile with:
+  //
+  //    cc styles.c -o styles -Wall -Werror -std=c17
+
+
+#include <stdio.h>
+
+
+enum textStyle  {
+  lowercase     = 1, // equivalent to 0x00000001
+  bold          = 2, // equivalent to 0x00000010
+  italic        = 4, // equivalent to 0x00000100
+  underline     = 8, // equivalent to 0x00001000
+  strikethrough = 16 // equivalent to 0x00010000
+};
+
+
+int main( void )  {
+  int style      = bold | italic;      // Style has bold and italic 
+                                       // turned on. 
+  int otherStyle = italic + underline; // OtherStyle has italic and  
+                                       // underline turned on. 
+  
+  if( style & bold ) 
+    printf( "For 'style' bold is turned on.\n");
+  if( !(otherStyle & bold) )
+    printf( "For 'otherStyel' bold is turned off\n" );
+  return 0;
+}
+
+  /*  eof  */
