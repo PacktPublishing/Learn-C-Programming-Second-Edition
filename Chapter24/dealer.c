fab0136e3ade89ceb9c28d5c3ab99a6181da2058
Jeff Szuhay <42553077+jeffszuhay@users.noreply.github.com>
2022-02-17 20:45:33 -0800

    Add files via upload

---
+++ Chapter24/dealer.c
@@ -0,0 +1,59 @@
+// dealer.c
+// Chapter 24
+// Learn C Programming, 2nd Edition
+//
+// Demonstrate 
+// 1) how to group data structures and functions that 
+//    manipulate them into .c/.h pairs. 
+// 2) use a common header file for all source files.
+// 3) use preprocessor directives to eliminate redundant include file preprocessing
+// 4) build a multi-file program 
+//
+// Dependencies:
+//
+//   card.h   card.c
+//   hand.h   hand.c
+//   deck.h   deck.c
+//
+// Compile with:
+//
+//    cc dealer.c card.c hand.c deck.c -o dealer -Wall -Werror -std=c17
+
+
+#include "dealer.h"
+
+extern bool bRandomize; // defined in deck.c
+
+int main( void )  {
+  Deck  deck;
+  Deck* pDeck = &deck;
+  
+  bRandomize = false;
+  
+  InitializeDeck( pDeck );
+  PrintDeck(      pDeck );
+  
+  ShuffleDeck( pDeck );
+  PrintDeck(   pDeck );
+  
+  Hand h1 , h2 , h3 , h4;
+  
+  Hand* hands[] = { &h1 , &h2 , &h3 , &h4 }; 
+  
+  for( int i = 0 ; i < kNumHands ; i++ )  {
+    InitializeHand( hands[i] );
+  }
+  
+  for( int i = 0 ; i < kCardsInHand ; i++ )  {
+    for( int j = 0 ; j < kNumHands ; j++ )  {
+      AddCardToHand( hands[j] , DealCardFromDeck( pDeck ) );
+    }
+  }
+  PrintAllHands( hands );
+  PrintDeck(     pDeck );
+  
+  return 0;
+}
+
+  /* eof */
+  
---
+++ Chapter24/deck.h
@@ -0,0 +1,36 @@
+// deck.h
+// Chapter 24
+// Learn C Programming, 2nd Edition
+//
+// Header file for deck.C
+//
+// To compile:
+//   see main program source file: dealer.c
+
+
+#ifndef _DECK_H_
+#define _DECK_H_
+
+
+#include "card.h"
+
+enum {
+  kCardsInDeck = 52
+};
+
+  // A Deck
+typedef struct {
+  Card  ordered[ kCardsInDeck ];
+  Card* shuffled[ kCardsInDeck ];
+  int   numDealt;
+  bool  bIsShuffled;
+} Deck;
+
+  // Operations on a Deck of Cards
+void  InitializeDeck(   Deck* pDeck );
+void  ShuffleDeck(      Deck* pDeck );
+Card* DealCardFromDeck( Deck* pDeck );
+void  PrintDeck(        Deck* pDeck );
+
+
+#endif
