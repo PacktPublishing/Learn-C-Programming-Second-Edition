538f5b51e0792d8f574e63f86377fc1c7b49b22a
Jeff Szuhay <42553077+jeffszuhay@users.noreply.github.com>
2022-03-25 02:55:49 -0700

    Add files via upload

---
+++ Chapter26/hand.h
@@ -0,0 +1,35 @@
+// hand.h
+// Chapter 26
+// Learn C Programming, 2nd Edition
+//
+// Header file for hand.c
+
+
+#ifndef _HAND_H_
+#define _HAND_H_
+
+
+#include <stdio.h>     // for printf()
+
+#include "card.h"
+
+
+enum {
+  kCardsInHand = 5,
+  kNumHands    = 4
+};
+
+  // A Hand
+typedef struct  {
+  int   cardsDealt;
+  Card* hand[ kCardsInHand ];
+} Hand;
+
+  // Operations on a Hand
+void InitializeHand( Hand* pHand );
+void AddCardToHand(  Hand* pHand , Card* pCard );
+void PrintHand(      Hand* pHand , char* pHandStr , char* pLeadStr );
+void PrintAllHands(  Hand* hands[ kNumHands ] );
+
+
+#endif 
