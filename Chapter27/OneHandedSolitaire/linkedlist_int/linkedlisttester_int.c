92965a90ba63478b63220e00587cca65b6bc7aea
Jeff Szuhay <42553077+jeffszuhay@users.noreply.github.com>
2022-07-12 18:08:23 -0700

    Add files via upload

---
+++ Chapter27/OneHandedSolitaire/linkedlist_int/linkedlisttester_int.c
@@ -0,0 +1,139 @@
+// linkedlisttester_int.c
+// Chapter 27
+// Learn C Programming, 2nd Edition
+//
+// This is an test program derived from linkedlisttester.c from
+// Chapter 18, herein called linked linkedlisttester_int_1file.c.
+// This test file does the same thing as linkedlisttester_int_1file.c.
+//
+// Notice that the use of test functions make this source file much smaller
+// than the companion verions, linkedlisttester_int_simple.c.
+//
+// Every library that you use or develop for yourself or others SHOULD
+// have a corresponding test program which exercises the library as 
+// commpletely as possible.
+//
+// compile with:
+//   cc linkedlisttester_int.c linkedlist_int.c -o linkedlisttester_int_simple -Wall -Werror -std=c17
+
+
+#include "linkedlist_int.h"
+
+
+// ======= For testing
+// Describe location of element being manipulated.
+//
+typedef enum  {
+  eFront = 0 ,
+  eBack
+} eWhere;
+
+// Describe acctions being performed upon the list.
+//
+typedef enum  {
+  eLook = 0 ,
+  eInsert ,
+  eDelete 
+} eAction;
+
+
+// ---- Test functions to exercise our linked list.
+
+void TestCreateNodeAndInsert( LinkedList* pLL , ListData data ,  eWhere where )  {
+  ListData* pData = CreateData( data );
+  ListNode* pNode = CreateNode( pData );
+  
+  switch( where ) {
+    case eFront: AddNode( pLL , pNode , 0  ); break;
+    case eBack:  AddNode( pLL , pNode , -1 ); break;
+  }
+}
+
+
+ListData TestExamineNode( LinkedList* pLL , eWhere where )  {
+  ListNode * pNode;
+  switch( where ) {
+    case eFront: pNode = GetNode( pLL , 0 ); break;
+    case eBack:  pNode = GetNode( pLL , pLL->nodeCount ); break;
+  }
+  ListData data = *(pNode->data);
+  return data;
+}
+
+
+ListData TestRemoveNodeAndFree( LinkedList* pLL , eWhere where )  {
+  ListNode * pNode;
+  switch( where ) {
+    case eFront: pNode = RemoveNode( pLL , 0  ); break;
+    case eBack:  pNode = RemoveNode( pLL , -1 ); break;
+  }
+  ListData data = *(pNode->data);
+  DeleteNode( pNode );
+  return data;
+}
+
+
+void TestPrintOperation( LinkedList* pLL , eAction action , 
+  ListData data , eWhere where )  {
+    switch( action )  {
+      case eLook:
+        data = TestExamineNode( pLL , where );
+        printf( "Get %s node, see [%2d].  " , 
+          where==eFront ? "front" : " back" , data );
+        break;
+      case eInsert:
+        printf( "Insert [%2d] to %s.      " , 
+          data , where==eFront ? "front" : " back" );
+        TestCreateNodeAndInsert( pLL , data , where );
+        break;
+      case eDelete:
+        data = TestRemoveNodeAndFree( pLL , where );
+        printf( "Remove [%2d] from %s.    " , 
+          data , where==eFront ? "front" : " back" );
+        break;
+      default:
+        printf( "::ERROR:: unknown action\n" );
+        break;
+    }
+    PrintList( pLL , PrintData );
+  }
+
+
+//=======================================================================
+
+int main( void )  {
+  LinkedList* pLL = CreateLinkedList();
+  
+  printf( "Input or operation         "
+    "Current state of linked list\n"
+    "=========================  "
+  "======================================");
+  printf( "\nUsing input{ 1  2  3  4 }. " );
+  int data1[]   = { 1 , 2 , 3 , 4 };
+  int data1Size = 4;
+  
+  PrintList( pLL , PrintData );
+  for( int i = 0 ; i < data1Size ; i++)  {
+    TestPrintOperation( pLL , eInsert , data1[i] , eFront );
+  }
+  
+  TestPrintOperation( pLL , eLook   , 0   , eFront );
+  TestPrintOperation( pLL , eDelete , 0   , eBack );
+  
+  printf( "\nUsing input{ 31 32 33 }.   " );
+  PrintList( pLL , PrintData );  
+  int data2[]   = { 31 , 32 , 33 };
+  int data2Size = 3;
+  for( int i = 0 ; i < data2Size ; i++)  {
+    TestPrintOperation( pLL , eInsert , data2[i] , eBack );
+  }
+  TestPrintOperation( pLL , eLook   , 0   , eBack );
+  
+  int count = pLL->nodeCount;
+  for( int i = 0 ; i < count ; i++)  {
+    TestPrintOperation( pLL , eDelete, 0 , eFront );
+  }
+}
+
+  /* eof */
+  
---
+++ Chapter27/OneHandedSolitaire/linkedlist_int/linkedlisttester_int_simple.c
@@ -0,0 +1,150 @@
+// linkedlisttester_int_simple.c
+// Chapter 27
+// Learn C Programming, 2nd Edition
+//
+// This is an alternative file to linkedlisttester_int.c.
+// This test file does the same thing as linkedlisttester_int.c.
+// Instead of using a bunch of test functions, it makes all of the 
+// calls directly to test them.
+//
+// Every library that you use or develop for yourself or others SHOULD
+// have a corresponding test program which exercises the library as 
+// commpletely as possible.
+//
+// compile with:
+//   cc linkedlisttester_int_simple.c linkedlist_int.c \
+//   -o linkedlisttester_int_simple -Wall -Werror -std=c17
+
+#include "linkedlist_int.h"
+
+
+// =================================================
+// main() function and
+
+int main( void )
+{
+  LinkedList* pLL = CreateLinkedList();
+  
+  ListData  data;
+  ListData* pData;
+  ListNode* pNode;
+  
+  printf( "Using int array[ 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10 ]\n\n" );
+  int dataArray[] = { 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10 };
+  int dataArraySize = 10;
+  
+  for( int i = 0 ; i < dataArraySize ; i++)  {
+    pData = (ListData*)calloc( 1 , sizeof( data ) );
+    *pData = dataArray[ i ];
+    pNode = CreateNode( pData );
+    AddNode( pLL , pNode , 0 );
+  }
+  
+  printf( "The Linked List has %d entries.  " , Size( pLL ) );
+  PrintList( pLL , PrintData );
+  printf( "\n\n" );
+
+  pNode = GetNode( pLL , 0 );
+  printf( "Examined 1st entry from list, value = " );
+  PrintNode( pNode, PrintData );
+  printf( "\n" );
+  printf( "The Linked List has %d entries. " , Size( pLL ) );
+  PrintList( pLL , PrintData );
+  printf( "\n\n" );
+
+  pNode = RemoveNode( pLL , 0 );
+  printf( "Removed 1st entry from list, value = " );
+  PrintNode( pNode, PrintData );
+  printf( "\n" );
+  printf( "The Linked List has %d entries.  " , Size( pLL ) );
+  PrintList( pLL , PrintData );
+  printf( "\n\n" );
+
+  pNode = RemoveNode( pLL , -1 );
+  printf( "Removed last entry from list, value = " );
+  PrintNode( pNode, PrintData );
+  printf( "\n" );
+  printf( "The Linked List has %d entries.  " , Size( pLL ) );
+  PrintList( pLL , PrintData );
+  printf( "\n\n" );
+
+  pNode = RemoveNode( pLL , 2 ); // 0-based indexing.
+  printf( "Removed 3rd entry from list, value = " );
+  PrintNode( pNode, PrintData );
+  printf( "\n" );  
+  printf( "The Linked List has %d entries.  " , Size( pLL ) );
+  PrintList( pLL , PrintData );
+  printf( "\n\n" );
+
+  pNode = RemoveNode( pLL , 1 ); // 0-based indexing.
+  printf( "Removed 2nd entry from list, value = " );
+  PrintNode( pNode, PrintData );
+  printf( "\n" );
+  printf( "The Linked List has %d entries.  " , Size( pLL ) );
+  PrintList( pLL , PrintData );
+  printf( "\n\n" );
+  
+  pNode = RemoveNode( pLL , 5 ); // 0-based indexing.
+  printf( "Removed 6th entry from list, value = " );
+  PrintNode( pNode, PrintData );
+  printf( "\n" );
+  printf( "The Linked List has %d entries.  " , Size( pLL ) );
+  PrintList( pLL , PrintData );
+  printf( "\n\n" );
+ 
+  pNode = RemoveNode( pLL , 3 ); // 0-based indexing.
+  printf( "Removed 4th entry from list, value = " );
+  PrintNode( pNode, PrintData );
+  printf( "\n" );
+  printf( "The Linked List has %d entries.  " , Size( pLL ) );
+  PrintList( pLL , PrintData );
+  printf( "\n\n" );
+
+  pData = (ListData*)calloc( 1 , sizeof( data ) );
+  *pData = 20;
+  pNode = CreateNode( pData );
+  AddNode( pLL , pNode , 0 );
+  printf( "Added to front of the list, value = %d\n" , 20 );
+  printf( "The Linked List has %d entries.  " , Size( pLL ) );
+  PrintList( pLL , PrintData );
+  printf( "\n\n" );
+
+  pData = (ListData*)calloc( 1 , sizeof( data ) );
+  *pData = 30;
+  pNode = CreateNode( pData );
+  AddNode( pLL , pNode , -1 );
+  printf( "Added to last element of the list, value = %d\n" , 30 );
+  printf( "The Linked List has %d entries.  " , Size( pLL ) );
+  PrintList( pLL , PrintData );
+  printf( "\n\n" );
+
+  pData = (ListData*)calloc( 1 , sizeof( data ) );
+  *pData = 40;
+  pNode = CreateNode( pData );
+  AddNode( pLL , pNode , 3 );
+  printf( "Added to 3rd element of the list, value = %d\n" , 40 );
+  printf( "The Linked List has %d entries.  " , Size( pLL ) );
+  PrintList( pLL , PrintData );
+  printf( "\n\n" );
+
+  pData = (ListData*)calloc( 1 , sizeof( data ) );
+  *pData = 11;
+  pNode = CreateNode( pData );
+  AddNode( pLL , pNode , 0 );
+  printf( "Added to front of the list, value = %d\n" , 11 );
+  printf( "The Linked List has %d entries.  " , Size( pLL ) );
+  PrintList( pLL , PrintData );
+  printf( "\n\n" );
+
+  pData = (ListData*)calloc( 1 , sizeof( data ) );
+  *pData = 12;
+  pNode = CreateNode( pData );
+  AddNode( pLL , pNode , 1 );
+  printf( "Added to 1st element of the list, value = %d\n" , 12 );
+  printf( "The Linked List has %d entries.  " , Size( pLL ) );
+  PrintList( pLL , PrintData );
+  printf( "\n\n" );
+}
+
+  // eof
+  
